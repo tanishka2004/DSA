6d4233e80b9ecc9d91b871b60a527cd3407aafcb
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-05-10 15:57:39 +0530

    Create rec_2.cpp

---
+++ basic-maths/rec_2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+    }
+};
+// Input
+// n =
+// 2
+// Output
+// 1
