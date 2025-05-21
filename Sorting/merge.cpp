e442859edc2f5edef5802850b859e152aad464f9
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-05-21 22:31:21 +0530

    Create merge.cpp

---
+++ Sorting/merge.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    // Helper function to merge two sorted arrays
+    vector<int> merge(vector<int>& left, vector<int>& right) {
+        vector<int> merged;
+        int i = 0, j = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left[i] <= right[j]) {
+                merged.push_back(left[i]);
+                i++;
+            } else {
+                merged.push_back(right[j]);
+                j++;
+            }
+        }
+
+        while (i < left.size()) {
+            merged.push_back(left[i]);
+            i++;
+        }
+
+        while (j < right.size()) {
+            merged.push_back(right[j]);
+            j++;
+        }
+
+        return merged;
+    }
+
+    // Main merge sort function
+    vector<int> mergeSort(vector<int>& nums) {
+        if (nums.size() <= 1)
+            return nums;
+
+        int mid = nums.size() / 2;
+        vector<int> left(nums.begin(), nums.begin() + mid);
+        vector<int> right(nums.begin() + mid, nums.end());
+
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left, right);
+    }
+};
