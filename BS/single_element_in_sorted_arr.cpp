141eaefe72db42276195df8c8113dcb6f4c3e1d4
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-06-21 16:24:23 +0530

    Update single_element_in_sorted_arr.cpp

--- BS/single_element_in_sorted_arr.cpp
+++ BS/single_element_in_sorted_arr.cpp
@@ -4,7 +4,7 @@ public:
         int n =nums.size();
         if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-1]) return nums[n-1];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
         int low = 1, high = n - 2;
         while(low<=high){
             int mid = (low+high)/2;
