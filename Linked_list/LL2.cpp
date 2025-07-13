6fd4eb8b03ac71b032d5215a24388bd93a88181a
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-07-13 21:59:47 +0530

    Create LL2.cpp

---
+++ Linked_list/LL2.cpp
@@ -0,0 +1,24 @@
+class Node {
+    public:
+        int data;         // Data stored in the node
+        Node* next;       // Pointer to the next node in the linked list
+        // Constructors
+        Node(int data1, Node* next1) {
+            data = data1;
+            next = next1;
+        }
+        // Constructor
+        Node(int data1) {
+            data = data1;
+            next = nullptr;
+        }
+};
+int main(){
+  vcetor<int> arr = {2,3,5,6};
+  // Create a Node 'x' with the first element of the vector and a null next reference
+    Node x = Node(arr[0], nullptr);
+    // Create a pointer 'y' pointing to the Node 'x'
+    Node* y = &x;
+    // Print the memory address of the Node 'x' using the pointer 'y'
+    cout << y << '\n';
+}
