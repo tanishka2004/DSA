e84e0d0304c8e537b014c53c0528d8fd2ddd2043
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-07-14 22:33:14 +0530

    Update del_node_in_LL.cpp

--- Linked_list/del_node_in_LL.cpp
+++ Linked_list/del_node_in_LL.cpp
@@ -13,3 +13,33 @@ public:
         node->next = node->next->next;
     }
 };
+
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    ListNode* deleteHead(ListNode* &head) {
+          if(head==NULL) return head;
+          ListNode* temp = head;
+          head = head->next;
+          delete temp;
+          return head;
+    }
+};
