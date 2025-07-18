977d62defec75e84fad264f596ba4cd3c9d92c04
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-07-18 10:51:04 +0530

    Update insertion_in_DLL.cpp

--- Linked_list/insertion_in_DLL.cpp
+++ Linked_list/insertion_in_DLL.cpp
@@ -35,3 +35,24 @@ public:
            return newNode;
     }
 };
+
+class Solution {
+  public:
+    // Function to insert a new node at given position in doubly linked list.
+    Node *addNode(Node *head, int pos, int data) {
+        Node* newNode = new Node(data);
+        Node* temp = head;
+        for(int i = 0; i < pos; i++){
+            if(temp == NULL) return head;
+            temp =  temp->next;
+        }
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        
+        if(temp->next != NULL){
+            temp->next->prev = newNode;
+        }
+        temp->next = newNode;
+        return head;
+    }
+};
