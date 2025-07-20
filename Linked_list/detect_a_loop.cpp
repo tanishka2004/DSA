482773adc53e516e4c6dcf391e67a46039155488
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-07-20 15:03:37 +0530

    Create detect_a_loop.cpp

---
+++ Linked_list/detect_a_loop.cpp
@@ -0,0 +1,37 @@
+/*
+Definition of singly linked list:
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = NULL;
+    }
+    ListNode(int data1)
+    {
+        val = data1;
+        next = NULL;
+    }
+    ListNode(int data1, ListNode *next1)
+    {
+        val = data1;
+        next = next1;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+       ListNode* slow = head;
+       ListNode* fast  = head;
+       while(fast !=  NULL && fast-> next != NULL){
+             slow = slow->next;
+             fast = fast->next->next;
+              if(fast == slow) return true;
+       }
+       return false;
+    }
+};
