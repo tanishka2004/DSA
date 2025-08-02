1918fc5cad2fb4ee3c8cc12c68ff968b6a28380c
Tanishka Jain <131334731+tanishka2004@users.noreply.github.com>
2025-08-02 20:53:38 +0530

    Create reverse_nodes_in_kth_groups.cpp

---
+++ Linked_list/reverse_nodes_in_kth_groups.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+ListNode* reverseLL(ListNode* head){
+    ListNode* temp = head;
+    ListNode* prev = NULL;
+
+    while(temp!=NULL){
+        ListNode* front = temp->next;
+        temp->next = prev;
+
+        prev = temp;
+
+        temp = front;
+    }
+    return prev;
+}
+ListNode* findkthNode(ListNode* temp, int k){
+    k -= 1;
+    while(temp  != NULL && k >0){
+        k--;
+        temp = temp->next;
+    }
+    return temp;
+}
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        ListNode* prevLast = NULL;
+        while(temp != NULL){
+            ListNode* kthNode = findkthNode(temp, k);
+            if(kthNode == NULL){
+                if(prevLast) prevLast->next = temp;
+                break;
+            }
+            ListNode* nextNode = kthNode->next;
+            kthNode->next = NULL;
+            reverseLL(temp);
+            if(temp == head){
+                head = kthNode;
+            }
+            else{
+                prevLast->next = kthNode;
+            }
+            prevLast = temp;
+            temp = nextNode;
+        }
+        return head;
+    }
+};
